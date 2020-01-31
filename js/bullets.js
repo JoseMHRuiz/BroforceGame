8c3ae61a0dccbf1acc4fae28ecf25b8326cc516b
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-01-31 11:07:58 +0100

    Buttons grennshock

---
+++ js/bullets.js
@@ -0,0 +1,42 @@
+class Bullets {
+    constructor(ctx, playerPosX, playerPosY, tarX, tarY, playerWidth, playerHeight) {
+        this.ctx = ctx;
+        this.posX = playerPosX + playerWidth;
+        this.posY = playerPosY + playerHeight / 2;
+        this.width = 14;
+        this.height = 14; 
+        this.targetX = tarX // Tarjer X&Y gets the number in the player
+        this.targetY = tarY
+
+        this.speed = 10 // Defines the speed for the trayectory
+
+        this.vecX = undefined; // defines the vector X & Y
+        this.vecY = undefined;
+        this.image = new Image();
+        this.image.src = './img/Bullet.png';
+    }
+
+    draw() {
+        this.ctx.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+
+        //     this.ctx.beginPath();
+        //     this.ctx.fillStyle = "whitesmoke";
+        //     this.ctx.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2);
+        //     this.ctx.fill();
+        //     this.ctx.closePath();
+    }
+
+    move() {
+        this.vecX = this.targetX - this.posX //This is the position of the target minus the position of the origin
+        this.vecY = this.targetY - this.posY 
+        let distance = Math.sqrt(this.vecX * this.vecX + this.vecY * this.vecY) //this return the SquareRoot of the ** of the vectors, distance
+        let moves = distance / this.speed // now we have the distance / with the speed
+        this.velX = (this.vecX) / moves; // here we define the velocity
+        this.velY = (this.vecY) / moves;
+        this.preVelX = this.vecX // this defines the previus point for eliminate the bullet later
+        this.preVelY = this.vecY
+        this.posX += this.velX // now we apply the velocity yo the position, and we have the movement!!
+        this.posY += this.velY
+    }
+
+}
---
+++ js/enemies.js
@@ -0,0 +1,81 @@
+class Enemy {
+    constructor(ctx, gameW, gameH, imgSource, ) {
+        this.ctx = ctx
+
+        this.gameWidth = gameW
+        this.gameHeight = gameH
+        this.width = 80;
+        this.height = 100;
+        this.posX = gameW;
+        this.posY = randomInt(200, 800)
+        this.live = 100;
+        this.image = new Image();
+        this.image.src = imgSource;
+        this.image.frames = 3; // the number of img of the sprite
+        this.image.framesIndex = 0;
+        this.enemyDeadArr = [];
+
+        this.speed = randomIntVel(0.2 , 2) // this set the speed of the zombie
+        // this.kills = kills
+    }
+
+    draw(framesCounter) {
+        this.ctx.drawImage( // this draw the enemy
+            this.image,
+            this.image.framesIndex * Math.round(this.image.width / this.image.frames), // divide the width of the sprite
+            0,
+            Math.floor(this.image.width / this.image.frames),
+            this.image.height,
+            this.posX,
+            this.posY,
+            this.width,
+            this.height
+        );
+        this.animate(framesCounter)
+        this.move()
+    }
+    animate(framesCounter) { // this animates the player
+        if (framesCounter % 18 == 0) {
+            this.image.framesIndex++;
+        }
+        if (this.image.framesIndex > this.image.frames - 1) {
+            this.image.framesIndex = 0;
+        }
+    }
+    animateDead(framesCounter) { // this animates the player
+        if (framesCounter % 18 == 0) {
+            this.image.framesIndex++;
+        // }
+        // if (this.image.framesIndex > this.image.frames - 1) {
+            // this.image.framesIndex = 0;
+        }
+    }
+
+    move() {
+        this.posX -= this.speed
+    }
+
+    drawDead(enemy, framesCounter) { //This draw the deadAnimation passing the parametres!!
+        this.ctx.drawImage( // this draw the enemy
+            this.image,
+            this.image.framesIndex * Math.round(this.image.width / this.image.frames), // divide the width of the sprite
+            0,
+            Math.floor(this.image.width / this.image.frames),
+            this.image.height,
+            this.posX,
+            this.posY,
+            this.width,
+            this.height
+        );
+        this.animateDead(framesCounter)
+        enemy.image.frames = 6
+        enemy.image.src = './img/Zombie2Deifinitive.png'
+        enemy.speed = 0
+        enemy.width = 110
+        enemy.height = 130
+        // if (this.framesCounter % 1000 === 0) this.framesCounter = 0
+    }
+    kill() {
+
+    }
+}
